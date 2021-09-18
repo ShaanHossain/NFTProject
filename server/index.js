f90ab6178b4a684c0f9c29aa8026262848e7af28
ShaanHossain <shaanhossain19@gmail.com>
2021-09-18 13:44:20 -0400

    stupid intelliJ, resetting master

--- server/index.js
+++ server/index.js
@@ -12,6 +12,11 @@ const app = express();
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 // Handle GET requests to /api route
+app.post("/api/nft", (req, res) => {
+  res.json({ message: "Hello from server!" });
+});
+
+// Handle GET requests to /api route
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
 });
